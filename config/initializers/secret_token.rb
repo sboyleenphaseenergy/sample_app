ac459141ac97b03940639513db6ac81a4b6f849a
Stephen Boyle <sboyle@enphaseenergy.com>
2012-11-25 21:14:28 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '611cf655e1f98eeb9fa57ff9674689df31947c747b99a07d60c0e89e725a285867f6934ceac316167dfa45b56a1aba7299ef147946bb252a24f9836261d3c885'
