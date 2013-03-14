76a8e6a3204c63e5b6c022210de8f5bfeb443724
Stephen Boyle <sboyle@enphaseenergy.com>
2013-03-14 11:17:56 -0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
+  
   def new
+    @user = User.new
   end
+  
+  def show
+    @user = User.find(params[:id])
+    #puts "TESTING:  #{@user.inspect()}" 
+    #This will appear in the rails server output or rspec output
+  end
+  
+  def create
+    @user = User.new(params[:user])
+    
+    #puts "TESTING: #{params.inspect}"
+    
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  
 end
